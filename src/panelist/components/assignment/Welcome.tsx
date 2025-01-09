6454f99013632edad98af041c76958dda97398bf
Bruno Oberle <bruno@boberle.com>
2025-01-09 13:48:17 +0100

    panelist app (welcome comp): change title

--- src/panelist/components/assignment/Welcome.tsx
+++ src/panelist/components/assignment/Welcome.tsx
@@ -12,7 +12,9 @@ const Welcome = ({ message, onClose, onStart }: WelcomeProps) => {
   return (
     <View style={styles.container}>
       <View style={styles.body}>
-        <Text style={styles.heading}>Survey time!</Text>
+        <Text style={styles.heading}>
+          Bienvenue à cette enquête sur Lang Track App!
+        </Text>
         <Text style={styles.message}>{message}</Text>
       </View>
       <View style={styles.footer}>
